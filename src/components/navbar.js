a7bf9527f954af0167cbbced297cf5ceec3a5c5b
ObayaDevOps <obayajamesdralega@gmail.com>
2024-01-18 17:48:16 +0300

    modified colour

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -29,13 +29,11 @@ export default function WithSubnavigation() {
   return (
     <Box>
       <Flex
-        bg={useColorModeValue('red', 'gray.800')}
-        color={useColorModeValue('gray.600', 'white')}
+        bg={useColorModeValue('red.600', 'gray.800')}
         minH={'60px'}
         py={{ base: 2 }}
         px={{ base: 4 }}
         borderStyle={'solid'}
-        borderColor={useColorModeValue('gray.200', 'gray.900')}
         align={'center'}>
         <Flex
           flex={{ base: 1, md: 'auto' }}
@@ -49,12 +47,7 @@ export default function WithSubnavigation() {
           />
         </Flex>
         <Flex flex={{ base: 1 }} justify={{ base: 'center', md: 'start' }}>
-          {/* <Text
-            textAlign={useBreakpointValue({ base: 'center', md: 'left' })}
-            fontFamily={'heading'}
-            color={useColorModeValue('gray.800', 'white')}>
-            Logo
-          </Text> */}
+
 
           <Flex display={{ base: 'none', md: 'flex' }} ml={10}>
             <DesktopNav />
@@ -226,12 +219,17 @@ const MobileNavItem = ({ label, children, href }) => {
 const NAV_ITEMS = [
   {
     label: 'Food',
-    href: '/menus/food-menu',
+    href: '/food',
     
   },
   {
     label: 'Drinks',
-    href: '/menus/food-menu',
+    href: '/drinks',
+
+  },
+  {
+    label: 'About',
+    href: '/about',
 
   },
 ]
--- src/pages/index.js
+++ src/pages/index.js
@@ -20,7 +20,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       
-      <Box bg={'red'} minH={'100vh'}>
+      <Box bg={'red.600'} minH={'100vh'}>
 
         <Box>
           <Navbar />
